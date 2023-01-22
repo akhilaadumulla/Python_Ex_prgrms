186b5fafdb770cb85b47f2bf483235666828729b
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-22 11:10:49 +0530

    Add files via upload

---
+++ sololearn.py
@@ -0,0 +1,41 @@
+# print(1 == 1 and 2 == 2)
+
+# print(1 == 1 and 2 == 3)
+
+# print(1 != 1 and 2 == 2)
+
+# print(2 < 1 and 3 > 6)
+
+
+# humidity = int(input())
+# if humidity>=40 and humidity<=60:
+#     print("norm")
+
+# print(1 == 1 or 2 == 2)
+
+# print(1 == 1 or 2 == 3)
+
+# print(1 != 1 or 2 == 2)
+
+# print(2 < 1 or 3 >  6)   
+
+# print(not 1 == 1)
+
+# print(not 1 > 7)
+# if not True:
+#    print("1")
+# elif not (1 + 1 == 3):
+#    print("2")
+# else:
+#    print("3")
+
+# print(False == False or True)
+
+# print(False == (False or True))
+
+# print((False == False) or True)
+
+if 1 + 1 * 3 == 6:
+   print("Yes")
+else:
+   print("No")
