3d8372a9868b5a70a94b954f525dd0a67a335def
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-23 18:34:14 +0530

    Add files via upload

---
+++ sololearn_03.py
@@ -0,0 +1,33 @@
+# nums = [10, 9, 8, 7, 6, 5]
+# nums[0] = nums[1] - 5
+# if 4 in nums:
+#   print(nums[3])
+# else:
+#   print(nums[4])
+
+# nums = [1, 2, 3]
+# print(not 4 in nums)
+# print(4 not in nums)
+# print(not 3 in nums)
+# print(3 not in nums)
+
+# words = ["hello"]
+# words.append("world")
+# print(words[1])
+# nums = [1, 3, 5, 2, 4]
+# print(len(nums))
+# letters = ["a", "b", "c"]
+# letters.append("d")
+# print(len(letters))
+
+# words = ["Python", "fun"]
+# index = 1
+# words.insert(index, "is")
+# print(words)
+
+nums = [9, 8, 7, 6, 5]
+nums.append(4)
+nums.insert(2, 11)
+print(len(nums))
+
+
