12a4cbd6ddf55e8440944fb49188e81adaa8aefb
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-23 18:33:06 +0530

    Add files via upload

---
+++ sololearn_02.py
@@ -0,0 +1,61 @@
+# x = 4
+# y = 2
+# if not 1 + 1 == y or x == 4 and 7 == 8:
+#   print("Yes")
+# elif x > y:
+#   print("No")
+
+# words = ["Hello", "world", "!"]
+# print(words[0])
+# print(words[1])
+# print(words[2]) 
+
+# empty_list = []
+# print(empty_list)
+
+# fruits = ["apple", "cherry", "banana", "kiwi", "lemon", "pear", "peach", "avocado"]
+# num = int(input())
+# if num < 0 or num > 7:
+#     print("Wrong number")
+# else:
+#     print(fruits[num])
+
+#     number = 3
+# things = ["string", 0, [1, 2, number], 4.56]
+# print(things[1])
+# print(things[2])
+# print(things[2][2])
+
+# m = [
+#     [1,2,3],
+#     [4,5,6]
+#     ]
+    
+# print(m[1][2]) 
+# num = [5, 4, 3, [2], 1]
+# print(num[0])
+# print(num[3][0])
+# print(num[5])
+# nums = [7, 7, 7, 7, 7]
+# nums[2] = 5
+# print(nums)nums = [1, 2, 3, 4, 5]
+# nums=[1,2,3,4,5]
+# nums[3] = nums[1]
+# print(nums[3])
+
+# nums = [1, 2, 3]
+# print(nums + [4, 5, 6])
+# print(nums * 3)
+
+# words = ["spam", "egg", "spam", "sausage"]
+# print("spam" in words)
+# print("egg" in words)
+# print("tomato" in words)
+
+items = [42, 88, 721, 12, 43, 22, 908]
+
+num = int(input())
+#your code goes here
+if num in items:
+    print("bingo")
+
