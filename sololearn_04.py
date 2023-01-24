6a4537e6f47f52ab40597364c486d29e34cbdee4
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-24 17:09:20 +0530

    Add files via upload

---
+++ sololearn_04.py
@@ -0,0 +1,85 @@
+# i = 1
+# while i <=5:
+#     print(i)
+#     i = i + 1
+
+# # print("Finished!")
+
+# # items = int(input())
+# # days = int(input())
+# # #your code goes here
+# # while days>0:
+# #     items*=2
+# #     days-=1
+
+# # print(items)    
+# # i = 3
+# # while i>=0:
+# #    print(i)
+# #    i = i - 1
+
+# x = 1
+# while x < 10:
+#     if x%2 == 0:
+#         print(str(x) + " is even")
+#     else:
+#         print(str(x) + " is odd")
+
+#     x += 1
+
+#     i = 0
+# while 1==1:
+#     print(i)
+#     i = i + 1
+#     if i >= 5:
+#         print("Breaking")
+#         break
+
+# print("Finished")
+
+# i = 5
+# while True:
+#   print(i)
+#   i = i - 1
+#   if i <= 2:
+#     break
+
+# i = 0
+# while i<5:
+#   i += 1
+#   if i==3:
+#     print("Skipping 3")
+#     continue
+#   print(i)
+
+# i = 0
+# while i<5:
+#   i += 1
+#   if i==3:
+#     print("Skipping 3")
+#     continue
+#   print(i)
+
+# str = "testing for loops"
+# count = 0
+
+# for x in str:
+#    if(x == 't'):
+#     count += 1
+
+# print(count)
+
+# list = [2, 3, 4, 5, 6, 7]
+
+# for x in list:
+#     if(x%2==1 and x>4):
+#         print(x)
+#         break
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+sum = 0
+#your code goes here
+for i in list:
+    sum+=i
+print(sum)
