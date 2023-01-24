9baaf39e56f7a4a2ef5a5fffb284d399fc491801
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-24 22:02:09 +0530

    Add files via upload

---
+++ sololearn_05.py
@@ -0,0 +1,33 @@
+# numbers = list(range(10))
+# print(numbers)
+
+# nums = list(range(5))
+# print(nums[4])
+
+# numbers = list(range(3, 8))
+# print(numbers)
+
+# print(range(20) == range(0, 20))
+# numbers = list(range(5, 20, 2))
+# print(numbers)
+
+# nums = list(range(3, 15, 3))
+# print(nums[2])
+
+# a = int(input())
+# b = int(input())
+# #your code goes here
+# numbers = list(range(a,b))
+# print(numbers)
+
+# list = [1, 1, 2, 3, 5, 8, 13]
+# print(list[list[4]])
+
+# for i in range(10):
+#   if not i % 2 == 0:
+#     print(i+1)
+
+letters = ['x', 'y', 'z']
+letters.insert(1, 'w')
+# print(letters[2])
+print(letters)
