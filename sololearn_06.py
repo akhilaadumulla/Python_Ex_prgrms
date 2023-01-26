029297471191440b36ada59e33a393d0c331279b
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-26 12:36:20 +0530

    Add files via upload

---
+++ sololearn_06.py
@@ -0,0 +1,33 @@
+# print("Hello world!")
+# range(2, 20)
+# str(12)
+# range(10, 20, 3)
+
+# def my_func():
+#     print("spam")
+#     print("spam")
+#     print("spam")
+
+# my_func()
+
+# hello()
+# def hello():
+#     print("Hello world!")
+
+# def welcome_message():
+# 	#redesign this function
+# 	name = input()
+# 	print("Welcome, "+name)
+# welcome_message()
+
+# def print_with_exclamation(word):
+#    print(word + "!")
+    
+# print_with_exclamation("spam")
+# print_with_exclamation("eggs")
+# print_with_exclamation("python")
+
+def print_double(x):
+   print(2 * x)
+
+print_double(3)
