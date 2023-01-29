725a5255361695d4cb0a6c0c0b909485f7c0d440
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-01-29 22:19:03 +0530

    Add files via upload

---
+++ sololearn_08.py
@@ -0,0 +1,28 @@
+# x = 365
+# y = 7
+# # this is a comment
+
+# print(x % y) # find the remainder
+# print (x // y)
+
+# def shout(word):
+#     """
+#     Print a word with an
+#     exclamation mark following it.
+#     """
+#     print(word + "!")
+
+# shout("spam")
+
+# def multiply(x, y):
+#     return x * y
+
+# a = 4
+# b = 7
+# operation = multiply
+# print(operation(a, b))
+def shout(word):
+   return word + "!"
+speak = shout
+output = speak("shout")
+print(output)
