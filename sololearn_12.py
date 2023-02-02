c795dfba1f4b1e68b08cfa7358004265933dc1b6
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-02-02 12:02:42 +0530

    Add files via upload

---
+++ sololearn_12.py
@@ -0,0 +1,44 @@
+# file = open("newfile.txt", "w")
+# file.write("This has been written to a file")
+# file.close()
+
+# file = open("newfile.txt", "r")
+# print(file.read())
+# file.close()
+
+# names = ["John", "Oscar", "Jacob"]
+
+# file = open("names.txt", "w+")
+# #write down the names into the file
+# for i in names:
+#     file.write(i+ "\n")
+# file.close()
+
+# file= open("names.txt", "r")
+# #output the content of file in console
+# print(file.read())
+
+# file.close()
+
+# try:
+#   print(1)
+#   print(20 / 0)
+#   print(2)
+# except ZeroDivisionError:
+#   print(3)
+# finally:
+#   print(4)
+
+# try:
+#   print(1)
+#   assert 2 + 2 == 5
+# except AssertionError:
+#   print(3)
+# except:
+#   print(4)
+
+foo = print()
+if foo == None:
+   print(1)
+else:
+   print(2)
