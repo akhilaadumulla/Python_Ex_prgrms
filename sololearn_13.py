55c2053319ef9f3edae4aed411a1eac74d68e786
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2023-02-03 17:52:19 +0530

    Add files via upload

---
+++ sololearn_13.py
@@ -0,0 +1,12 @@
+# def power(x, y):
+#   if y == 0:
+#     return 1
+#   else:
+#     return x * power(x, y-1)
+		
+# print(power(2, 3))
+
+nums = {1, 2, 3, 4, 5, 6}
+nums = {0, 1, 2, 3} & nums
+nums = filter(lambda x: x > 1, nums)
+print(len(list(nums)))
